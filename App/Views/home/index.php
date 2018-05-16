ff8c1a76188f6af88194f7a1c5b1bea1ca8a3bcd
Machar1el <smt005@gmail.com>
2018-05-16 15:28:05 +0200

    Ajout du tchat

--- App/Views/home/index.php
+++ App/Views/home/index.php
@@ -1,1 +1,18 @@
 <h2 class="center">Page Index</h2>
+
+<div class="chatbox">
+  <div class="chatboxheader">Support technique</div>
+
+  <div class="chatboxMessages">
+
+    <span class="distantMessage">Bonjour, je suis Olivier du Support Pôle Emploi, en quoi puis-je vous être utile ?</span>
+    <span class="localMessage">Help me ! J'essaie de faire l'inscription initiale,
+    mais je n'y arrive pas...</span>
+    <span class="distantMessage">Ne vous inquiétez pas %USER_LASTNAME%, ça va bien se passer ;)</span>
+
+  </div>
+  <div class="chatboxFooter">
+    <input class="chatboxInput" type="text" placeholder="Répondre..."></input>
+    <img src="https://png.icons8.com/color/260/send-letter.png" height="25px" width="25px">
+  </div>
+</div>
